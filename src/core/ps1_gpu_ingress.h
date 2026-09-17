ba2a5525cd6d9180063c2389ce30c942b8534351
TheTeu <kingmath3us@gmail.com>
2026-09-17 11:45:12 -0300

    feat: add PS1 GPU VRAM transfer state

--- src/core/ps1_gpu_ingress.h
+++ src/core/ps1_gpu_ingress.h
@@ -2,6 +2,7 @@
 
 #include "core/r3000a_bus.h"
 
+#include <array>
 #include <cstdint>
 #include <optional>
 
@@ -9,21 +10,47 @@ namespace jojo {
 
 class Ps1GpuIngress {
 public:
+    static constexpr std::uint32_t vram_width = 1024u;
+    static constexpr std::uint32_t vram_height = 512u;
+
     [[nodiscard]] R3000aBusResult write_gp0(std::uint32_t value) noexcept;
     [[nodiscard]] R3000aBusResult write_gp1(std::uint32_t value) noexcept;
 
     [[nodiscard]] std::uint32_t status() const noexcept;
     [[nodiscard]] std::uint64_t gp0_word_count() const noexcept;
     [[nodiscard]] std::uint64_t gp1_command_count() const noexcept;
+    [[nodiscard]] std::uint16_t vram_pixel(std::uint32_t x, std::uint32_t y) const noexcept;
+    [[nodiscard]] std::uint64_t vram_write_count() const noexcept;
     [[nodiscard]] const std::optional<std::uint8_t>& last_unsupported_gp0_command() const noexcept;
     [[nodiscard]] const std::optional<std::uint8_t>& last_unsupported_gp1_command() const noexcept;
 
 private:
     static constexpr std::uint32_t reset_status = 0x14802000u;
 
+    enum class Gp0Mode : std::uint8_t {
+        command,
+        cpu_to_vram_destination,
+        cpu_to_vram_size,
+        cpu_to_vram_payload,
+    };
+
+    void reset_command_buffer() noexcept;
+    void write_transfer_pixel(std::uint16_t pixel) noexcept;
+
     std::uint32_t status_{reset_status};
     std::uint64_t gp0_word_count_{};
     std::uint64_t gp1_command_count_{};
+    std::array<std::uint16_t, vram_width * vram_height> vram_{};
+    std::uint64_t vram_write_count_{};
+
+    Gp0Mode gp0_mode_{Gp0Mode::command};
+    std::uint32_t transfer_x_{};
+    std::uint32_t transfer_y_{};
+    std::uint32_t transfer_width_{};
+    std::uint32_t transfer_height_{};
+    std::uint32_t transfer_pixel_index_{};
+    std::uint32_t transfer_pixels_remaining_{};
+
     std::optional<std::uint8_t> last_unsupported_gp0_command_{};
     std::optional<std::uint8_t> last_unsupported_gp1_command_{};
 };
