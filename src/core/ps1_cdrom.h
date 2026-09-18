4c6328039586336937ce6a875c0abf3fea5b16c8
TheTeu <kingmath3us@gmail.com>
2026-09-18 02:44:58 -0300

    feat: expose bounded CD-ROM command evidence

--- src/core/ps1_cdrom.h
+++ src/core/ps1_cdrom.h
@@ -11,6 +11,12 @@
 
 namespace jojo {
 
+struct Ps1CdromCommandEvent {
+    std::uint8_t command{};
+    std::uint8_t index{};
+    std::uint8_t status{};
+};
+
 class Ps1CdromController {
 public:
     void attach_disc(const Ps1DiscSession* disc) noexcept;
@@ -22,12 +28,15 @@ public:
     [[nodiscard]] std::size_t data_bytes_available() const noexcept;
     [[nodiscard]] std::size_t read_data_words(std::span<std::uint32_t> out) noexcept;
     [[nodiscard]] std::uint64_t current_lba() const noexcept;
+    [[nodiscard]] std::uint64_t command_count() const noexcept;
+    [[nodiscard]] const std::deque<Ps1CdromCommandEvent>& recent_commands() const noexcept;
     [[nodiscard]] const std::optional<std::uint8_t>& last_unsupported_command() const noexcept;
 
 private:
     static constexpr std::size_t parameter_capacity = 16u;
     static constexpr std::size_t response_capacity = 16u;
     static constexpr std::size_t data_capacity = 2048u;
+    static constexpr std::size_t command_history_capacity = 64u;
 
     [[nodiscard]] bool push_response(std::uint8_t value) noexcept;
     [[nodiscard]] R3000aBusResult execute_command(std::uint8_t command) noexcept;
@@ -42,6 +51,8 @@ private:
     std::deque<std::uint8_t> parameters_{};
     std::deque<std::uint8_t> responses_{};
     std::deque<std::uint8_t> data_{};
+    std::uint64_t command_count_{};
+    std::deque<Ps1CdromCommandEvent> recent_commands_{};
     std::optional<std::uint8_t> last_unsupported_command_{};
 };
 
