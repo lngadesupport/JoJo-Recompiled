58ba63dbde771c7176c3ac7501ea9708c69f81e6
TheTeu <kingmath3us@gmail.com>
2026-09-20 14:55:35 -0300

    migration: model TKC 10-byte leaf records

--- src/content/fighter_tk.h
+++ src/content/fighter_tk.h
@@ -5,16 +5,23 @@
 #include <array>
 #include <cstdint>
 #include <span>
+#include <vector>
 
 namespace jojo::content {
 
 constexpr std::uint32_t fighter_tkc_load_base = 0x8010D800u;
 constexpr std::size_t fighter_tk_slot_count = 27u;
 
+struct FighterTkRecord {
+    std::uint32_t source_offset{};
+    std::array<std::uint16_t, 5> fields{};
+};
+
 struct FighterTkSlot {
     bool tkc_null{};
     std::uint32_t tkc_offset{};
     std::uint32_t tkd_value{};
+    std::vector<FighterTkRecord> tkc_records;
 };
 
 struct FighterTkRoots {
