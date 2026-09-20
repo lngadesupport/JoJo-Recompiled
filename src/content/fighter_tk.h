1be90266e69fad203c702a980bfa90a9b868ee9b
TheTeu <kingmath3us@gmail.com>
2026-09-20 15:02:42 -0300

    migration: correct TKC/TKD retail slot structure

--- src/content/fighter_tk.h
+++ src/content/fighter_tk.h
@@ -10,24 +10,31 @@
 namespace jojo::content {
 
 constexpr std::uint32_t fighter_tkc_load_base = 0x8010D800u;
-constexpr std::size_t fighter_tk_slot_count = 27u;
+constexpr std::size_t fighter_tk_slot_count = 26u;
 
-struct FighterTkRecord {
+struct FighterTkcRecord {
     std::uint32_t source_offset{};
     std::array<std::uint16_t, 5> fields{};
 };
 
+struct FighterTkdRecord {
+    std::uint32_t source_offset{};
+    std::array<std::int16_t, 4> fields{};
+};
+
 struct FighterTkSlot {
-    bool tkc_null{};
     std::uint32_t tkc_offset{};
-    std::uint32_t tkd_value{};
-    std::vector<FighterTkRecord> tkc_records;
+    std::vector<FighterTkcRecord> tkc_records;
+    std::uint32_t tkd_offset{};
+    std::vector<FighterTkdRecord> tkd_records;
 };
 
 struct FighterTkRoots {
     std::array<FighterTkSlot, fighter_tk_slot_count> slots{};
     std::uint32_t tkc_size{};
+    std::uint32_t tkc_end_offset{};
     std::uint32_t tkd_size{};
+    std::uint32_t tkd_block_size{};
 };
 
 [[nodiscard]] Result<FighterTkRoots> parse_fighter_tk_roots(
